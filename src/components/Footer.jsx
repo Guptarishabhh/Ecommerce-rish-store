6785bc979934db9d71fdb09bf10a2c846bb9dab0
Guptarishabhh <rishabhguptass123@gmail.com>
2026-05-02 23:33:54 +0530

    kjh

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -25,7 +25,7 @@ const Footer = () => {
               <span className="animate-ping absolute inline-flex h-full w-full rounded-full bg-green-400 opacity-75"></span>
               <span className="relative inline-flex rounded-full h-2 w-2 bg-green-500"></span>
             </span>
-            <span className="text-xs text-gray-300">Open to Work</span>
+            <span className="text-xs text-gray-300"> Open to Work </span>
           </div>
 
         </div>
